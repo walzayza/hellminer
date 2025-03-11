5c2c60c8aa6d6351801373f9cf1d017bce8a0d2a
walzayza <63353458+walzayza@users.noreply.github.com>
2025-03-11 22:31:48 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://de.vipor.net:5040 -u RKC7nw1V1dSgvRUwkzzPiaS1cvLNVi1zbM.oracle -p x --cpu "$(nproc)" --no-colors
+./hellminer -c stratum+tcp://us.vipor.net:5040 -u RKC7nw1V1dSgvRUwkzzPiaS1cvLNVi1zbM.oracle -p x --cpu "$(nproc)" --no-colors
